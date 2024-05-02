8e4f8d27e1497cc3aaf1391d831ab8f157c31d50
Vann <111390478+Van-x2@users.noreply.github.com>
2024-05-02 22:38:54 +0000

    finished template

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # SvelteTailwindSetup
+
+A Template that adds SvelteKit + TailwindCSS + Docker for easy project creation.
---
+++ Template/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+
+<h1 class="text-3xl font-bold text-blue-600 text-center">
+    A Simple Template With TailwindCSS + Sveltekit
+  </h1>
+  
